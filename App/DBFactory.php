40b6d230cfe23744a7c0cba2235a9ccb7424b238
Benjamin-ev <benjamin.evain4@gmail.com>
2023-04-12 23:00:10 +0200

    Update DBFactory.php

--- App/DBFactory.php
+++ App/DBFactory.php
@@ -1,7 +1,7 @@
 <?php
 class DBFactory{
 	public static function getMysqlConnexionWithPDO(){
-		$db = new PDO('mysql:host=*host*;dbname=*dbname*','*user*','*pwd*');
+		$db = new PDO('mysql:host=10.1.0.15;dbname=carteBandes','nodejs','boquette1815');
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		return $db;
 	}
